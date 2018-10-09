bf63076b4d911b5cf314b2a483249a1778fccc3e
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-10-09 12:53:56 -0400

    Add files via upload

---
+++ loops-master/3-mixed-practice/MixedLoopPractice.java
@@ -0,0 +1,86 @@
+/*
+ * APCS-A - Mixed Loop Practice
+ *
+ * This program is to give you practice writing methods that use for loops. Your
+ * task is to study the code and output for methods that I have completed. Then 
+ * you should complete and test the methods that are incomplete or missing. 
+ *
+ *  1) oneToThirtyF    - uses a for loop to print the set {1, 2, 3, ... , 30}.
+ *
+ *  2) oneToThirtyW    - uses a while loop to print the set {1, 2, 3, ... , 30}.
+ *
+ *  3) twentyToZeroF   - uses a for loop to print the set {20, 18, 16, ... , 0}.
+ *
+ *  4) twentyToZeroW   - uses a while loop to print the set {20, 18, 16, ... , 0}.
+ *
+ *  5) countMtoN       - uses a while loop to print the set {m, m+1, m+2, ... , n}.
+ *
+ *  6) sumN            - uses a while loop to find and return the sum of the first 
+ *                       n positive integers. 
+ *                       
+ *  7) perfectSquaresF - uses a for loop to print the first n perfect squares.
+ *
+ *  8) sumSquaresW     - uses a while loop find and return the sum of the first n 
+ *                       perfect squares.
+ *
+ *  9) iLoveJava       - uses a while loop to print the sentence "I love java." n
+ *                       times.
+ *
+ * 10) divBy2and9      - uses a for loop to count from 1 to 100 by ones, but only
+ *                       values that are evenly divisible by 2 and 9 are printed.
+ *                       (you'll need to use an if statement.)
+ *
+ * 11) divBy3and8      - uses a while loop to count from 1 to 100 by ones, but only
+ *                       values that are evenly divisible by 3 and 8 are printed.
+ *                       (you'll need to use an if statement.)
+ *
+ * 12) divBy5or7Sum    - uses a while loop to find and return the sum of all values in 
+ *                       the set {1, 2, 3, ... , 10000} that are divisible by 5 or 7.
+ *
+ * 13) fizzBuzz        - uses a while loop to count from 1 to n by ones. However,
+ *                       if a number is divisible by 3, the word "bizz" should be 
+ *                       printed, and if the number is divisible by 5, "buzz" should
+ *                       be printed. Sample output:
+ *
+ *                       1, 2, Fizz, 4, Buzz, Fizz, 7, 8, Fizz, Buzz, 11, Fizz, 13, 14, 
+ *                       Fizz Buzz, 16, 17, Fizz, 19, Buzz, Fizz, 22, 23, Fizz, ...
+ *                       
+ * 14) sumDigits       - finds and return the sum of the digits in an int.
+ *
+ * 15) countEvens      - finds and returns the number of even digits in an int.
+ *
+ * 16) hasDigit        - return true if a number n contains the digit d and false otherwise.
+ *
+ * 17) hasMoreEvens    - returns true if a number n has more even digits than odd digits
+ *                       and false otherwise.
+ *
+ *  ------------------------------------------------------------------------------
+ *
+ * @author  your name 
+ * @version date
+ */
+ 
+ public class MixedLoopPractice
+ {
+    public static void oneToThirtyF()
+    {
+        
+    }
+    
+    public static void oneToThirtyW()
+    {
+        
+    }
+        
+    // keep making methods here
+    
+    
+    public static void main(String[] args)
+    {
+        oneToThirtyF();
+        oneToThirtyW();
+        
+        // test your methods here
+    }
+ }
+ 
