bee69f05b2d3c9959a1d308045e08d160041582e
ewilbe5944 <32745881+ewilbe5944@users.noreply.github.com>
2018-10-12 12:56:40 -0400

    Add files via upload

--- loops-master/4-string-processing/StringProcessingPractice.java
+++ loops-master/4-string-processing/StringProcessingPractice.java
@@ -72,13 +72,13 @@
  *
  *  ------------------------------------------------------------------------------
  *
- * @author  your name 
- * @version date
+ * @author  Emily Wilber
+ * @version Oct 12, 2018
  */
  
  public class StringProcessingPractice
  {
-    /* The function testGrader works by comparing two strings, answers and key. 
+    /*  1) The function testGrader works by comparing two strings, answers and key. 
      *    answers will be a string of letters and possibly ? characters which represent 
      *    unanswered questions. key is the corresponding string of correct answers. 
      *    Grades are calculated by awarding 1 point for correct answers, no points for 
@@ -87,14 +87,13 @@
      *    given a score of 3.75.
      * 
      */
-    public static int testGrader(String answers, String key) {
+    public static double testGrader(String answers, String key) {
         int grade = 0;
-        int i;
-        for (i=0; i<key.length(); i++){
-            if (answers.indexOf(i) == key.indexOf(i)){
+        for (int i=0; i<key.length(); i++){
+            if (answers.charAt(i) == (key.charAt(i))){
                 grade++;
             }
-            else if ((key.indexOf(i)) == "?") {
+            else if (key.charAt(i) == ('?')) {
                 grade = grade;
             }
             else {
@@ -104,10 +103,85 @@
         return grade;
     }
     
+    /* 2. The function letterSwapper takes a string str as input and returning a new 
+     *    string with each pair of letters swapped. In the case of an odd number of 
+     *    characters in the string, the last character is simply appended to the end. 
+     *    For example passing a string of loops would return a string of olpos. 
+     *     
+     *          
+     */
+    public static String letterSwapper(String str) { 
+        String swapStr = "";
+        int i;
+        char a;
+        char b;
+        if (str.length() % 2 == 0) {
+            for (i = 0; i <= (str.length() - 2); i+= 2) {
+                a = str.charAt(i);
+                b = str.charAt(i + 1);
+                swapStr = swapStr + b + a;
+            }
+        }
+        else{
+            for (i = 0; i <= (str.length() - 1); i+= 2) {
+                a = str.charAt(i);
+                b = str.charAt(i + 1);
+                swapStr = swapStr + b + a;
+            }
+            swapStr = swapStr + str.substring(str.length() - 1);
+        }
+        return swapStr;
+    }
     
+    /*
+     *   3. The function caseComparer returns true if the input string contains the same 
+     *    number of lowercase letters as it does uppercase and false otherwise. The 
+     *    string may contain non-alphabetic characters. (Uppercase letters have ASCII 
+     *    values in the range 65 to 90 and lowercase letters values in the range 
+     *    97 to 122.)
+     *     
+     */
+    public static boolean caseComparer(String str) { 
+        int upper = 0;
+        int lower = 0;
+        for (int i = 0; i < str.length(); i++) {
+            if ((int) str.charAt(i) >= 65 && (int) str.charAt(i) <= 90) {
+                upper++; 
+            }
+            else {
+                lower++;
+            }
+        }
+        return (upper == lower); 
+    }
+     
+    /* 7. A starChecker takes a string str and returns true if for each occurrence of a * character, 
+     *    the characters on either side of the star are the same and false otherwise. If a * happens
+     *    to be at the end of the string, then it should return false.
+     */  
+    public static boolean starChecker(String str) { 
+        if (str.charAt(0) == '*' || str.charAt(str.length() - 1) == '*') {
+            return false;
+        }
+        
+        for (int i = 1; i < str.length() - 2; i++) {
+            if (str.charAt(i) == '*' && str.charAt(i-1) == str.charAt(i+1)) {
+                return true;
+            }
+        }
+        return false;
+        
+        
+    }
+           
     public static void main(String[] args)
     {
-        System.out.println("");
+        // System.out.println(testGrader("AAA?", "AABA"));
+        // System.out.println(letterSwapper("bobo"));
+        // System.out.println(letterSwapper("bobob"));
+        // System.out.println(caseComparer("Aaa"));
+        System.out.println("false " + starChecker("*aaa"));
+        System.out.println("true " + starChecker("a*a"));
     }
  }
  
